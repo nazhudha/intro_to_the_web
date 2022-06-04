e493722b724ca7b339d0b710d6b383cfb6cad532
nazhudha <57568434+nazhudha@users.noreply.github.com>
2022-06-04 20:19:08 +0100

    intro to web views

--- battle/spec/features/enter_names_spec.rb
+++ battle/spec/features/enter_names_spec.rb
@@ -1,12 +1,6 @@
 feature 'Enter names' do
   scenario 'submitting names' do
-    visit('/')
-    fill_in :player_1_name, with: 'Charlotte'
-    fill_in :player_2_name, with: 'Mittens'
-    click_button 'Submit'
-
-    save_and_open_page
-
+    sign_in_and_play
     expect(page).to have_content 'Charlotte vs. Mittens'
   end
 end
