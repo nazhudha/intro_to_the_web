5f819980b40d81a3e053b24a9a14ea36098c5d7d
nazhudha <57568434+nazhudha@users.noreply.github.com>
2022-06-04 21:04:53 +0100

    intro to web views

--- battle/app.rb
+++ battle/app.rb
@@ -1,5 +1,6 @@
 require 'sinatra/base'
 require 'sinatra/reloader'
+require './lib/player.rb'
 
 class Battle < Sinatra::Base
   configure :development do
@@ -13,24 +14,23 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    session[:player_1_name] = params[:player_1_name]
-    session[:player_2_name] = params[:player_2_name]
+    $player_1 = Player.new(params[:player_1_name])
+    $player_2 = Player.new(params[:player_2_name])
     redirect '/play'
   end
-
+  
   get '/play' do
-    @player_1_name = session[:player_1_name]
-    @player_2_name = session[:player_2_name]
+    @player_1_name = $player_1.name
+    @player_2_name = $player_2.name
     erb :play
   end
-
+  
   get '/attack' do
-    @player_1_name = session[:player_1_name]
-    @player_2_name = session[:player_2_name]
+    @player_1 = $player_1
+    @player_2 = $player_2
+    @player_1.attack(@player_2)
     erb :attack
   end
-
-
 
   # start the server if ruby file executed directly
   run! if app_file == $0
--- battle/spec/features/attack_spec.rb
+++ battle/spec/features/attack_spec.rb
@@ -1,7 +1,11 @@
 feature 'Attacking' do
-  scenario 'attack Player 2' do
+  # 'attack Player 2' scenario omitted
+
+  scenario 'reduce Player 2 HP by 10' do
     sign_in_and_play
-    click_link 'Attack'
-    expect(page).to have_content 'Charlotte attacked Mittens'
+    click_button 'Attack'
+    click_link 'OK'
+    expect(page).not_to have_content 'Mittens: 60HP'
+    expect(page).to have_content 'Mittens: 50HP'
   end
 end
